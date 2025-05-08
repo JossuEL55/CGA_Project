65646f713deb5448955630b2563bb7493ff7ed6a
JossuEL55 <jossueayala555@gmail.com>
2025-05-07 23:31:53 -0500

    feat:: add migrations

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-migrations', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        return '✅ Migraciones ejecutadas con éxito.';
+    } catch (Exception $e) {
+        return '❌ Error al ejecutar migraciones: ' . $e->getMessage();
+    }
+});
