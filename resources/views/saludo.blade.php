aea9c7ba9c6d77fd49b830fcd8d08b48a5fcdacf
Bryan12sd <bryanzeta17@gmail.com>
2025-05-31 17:04:32 -0500

    feat: add changes for views and router

---
+++ resources/views/saludo.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pantalla de Saludo</title>
+</head>
+
+<body>
+    <h1>Hola Bryan, ¡bienvenido a tu pantalla personalizada con controlador!</h1>
+</body>
+
+</html>
